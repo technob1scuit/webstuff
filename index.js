5d2af129d2ffd992aad098b34608b39a223b9688
George-801 <56756124+George-801@users.noreply.github.com>
2020-12-12 22:01:04 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="description" content="webstuff yee">
+    <meta name="theme-color" content="#0085a1">
+	  
+    <meta property="og:title" content="webstuff">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://george-801.github.io/webstuff">
+    <meta property="og:description" content="webstuff yee">
+    <meta charset="utf-8">
+    <script>console.log(["webstuff", "webstuff yee"]);</script>
+    <style>
+      body {
+        font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+      }
+    </style>
+  </head>
+  <body>
+    <h1>webstuff</h1>
+    <hr>
+    <p>webstuff yee</p>
+    <img src="https://george-801.github.io/webstuff/reason.png" style="margin-bottom: 10px;">
+    <img src="https://george-801.github.io/webstuff/reason-2.png" style="padding-bottom: 20px;">
+  </body>
+</html>
